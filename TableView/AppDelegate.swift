b40107e208e014bc9bc00fde1678a35d0dc7d4e3
吉田敏幸 <toshiyuki@yoshidatoshiyuki-no-iMac.local>
2014-12-14 04:54:05 +0900

    Initial Commit

---
+++ TableView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TableView
+//
+//  Created by 吉田敏幸 on 2014/12/14.
+//  Copyright (c) 2014年 Toshiyuki Yoshida. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TableView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by 吉田敏幸 on 2014/12/14.
+//  Copyright (c) 2014年 Toshiyuki Yoshida. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
